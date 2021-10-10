f165e5da41ae28ed011c315adeb6ba2d9d57ed73
Daria-Malakhova-Chemoinf <dmalakhova89@yandex.ru>
2021-10-10 21:16:58 +0300

    3 HW Tasks

---
+++ 03_string-boolean-conditionals/HW3_2_Task_Malakhova.js
@@ -0,0 +1,23 @@
+//Задание 2
+//Задание:
+/*В переменных name, surname написаны имя и фамилия человека. При этом в строках беспорядок с большими и маленькими буквами,
+и нужно привести строки в порядок. Для этого первые буквы имени и фамилии приведите к верхнему регистру, а оставшиеся буквы —
+к нижнему. Запишите результат в новые переменные и выведите их значения с помощью console.log. С помощью тернарных операторов и
+console.log выведите сообщение «Имя было преобразовано» или «Имя осталось без изменений» для имени и фамилии в зависимости от того,
+были ли исходные строки равны преобразованным.*/
+
+let name, surname;
+
+//name = "DaRiA";
+//surname = "MaLaKhOvA";
+name = "DariA";
+surname = "MalakhovA";
+
+
+nameNew = name[0].toUpperCase() + name.substring(1).toLowerCase()
+surnameNew = surname[0].toUpperCase() + surname.substring(1).toLowerCase()
+if (name === nameNew && surname === surnameNew) {
+    console.log(`Имя ${name} ${surname} осталось без изменений`);
+} else {
+    console.log(`Имя ${name} ${surname} было преобразовано ${nameNew} ${surnameNew}`);
+}
