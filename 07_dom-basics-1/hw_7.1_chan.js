c37c4e89385073edd2bcdc1e8993ee5e2dc46e1c
nvulym <79202444+nvulym@users.noreply.github.com>
2021-11-29 00:34:53 +0300

    HW_7_Chan

---
+++ 07_dom-basics-1/hw_7.1_chan.js
@@ -0,0 +1,17 @@
+// Задание 1
+
+function stop() {
+    window.clearInterval(window.timerId);
+}
+function start() {
+    stop()
+    document.getElementById('timer').textContent = document.getElementById('inp-duration').value;
+    window.timerId = window.setInterval(timer, 1000);
+}
+function timer() {
+    let seconds = document.getElementById('timer');
+    seconds.textContent = parseInt(seconds.textContent) - 1;
+    if (parseInt(seconds.textContent) === 0) {
+        stop();
+    }
+}
